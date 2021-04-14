56f5ad7204ca9b18f9b27c631202cda1b2340f0d
Takayuki <taka19950309@gmail.com>
2021-04-14 17:28:13 +0900

    環境変数設定

--- e-management/app/Providers/AppServiceProvider.php
+++ e-management/app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (\App::envrionment('production')) {
-            \URL::forceScema('https');
+            \URL::forceScheme('https');
         }
     }
 }
